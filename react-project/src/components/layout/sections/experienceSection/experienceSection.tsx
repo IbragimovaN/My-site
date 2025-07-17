d2e4b34e09b65ba8b047bf5d76435e8472df1ea4
Anastasiya <anastasiya.ibr@mail.ru>
2025-07-17 16:24:06 +0300

    feat: заполнила experience

--- react-project/src/components/layout/sections/experienceSection/experienceSection.tsx
+++ react-project/src/components/layout/sections/experienceSection/experienceSection.tsx
@@ -1,15 +1,40 @@
 import { SECTIONS_DATA } from "../../../../constants";
+import { EXPERIENCE_DATA } from "../../../../constants/experions-data";
 import { SectionTitle } from "../../../ui";
 import type { SectionProp } from "../types";
 import styles from "./experienceSection.module.scss";
 export const ExperienceSection = ({ id }: SectionProp) => {
   return (
-    <section id={id} className="sectionsWrapper">
+    <section id={id} className={`sectionsWrapper ${styles.wrapper}`}>
       <SectionTitle title={SECTIONS_DATA.experience.sectionTitle} />
       <div className={styles.container}>
-        <div className={styles.backgroundImage}>
-          <ul className={styles.infoBox}>
+        <ul className={styles.infoBox}>
+          {EXPERIENCE_DATA.map((item) => (
             <li className={styles.infoItemWrapper}>
+              <div className={styles.infoItem}>
+                <div>{item.date}</div>
+                <h4> {item.school}</h4>
+                {item.name && <h3> {item.name}</h3>}
+                <p>{item.descr}</p>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className={styles.icon}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+                  />
+                </svg>
+              </div>
+            </li>
+          ))}
+          {/* <li className={styles.infoItemWrapper}>
               <div className={styles.infoItem}>
                 <h4> РГГРУ</h4>
                 <p>descr</p>
@@ -77,9 +102,8 @@ export const ExperienceSection = ({ id }: SectionProp) => {
                   />
                 </svg>
               </div>
-            </li>
-          </ul>
-        </div>
+            </li>     */}
+        </ul>
       </div>
     </section>
   );
