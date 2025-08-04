c05ebe9186e5896fee2092f2988142ddee7d1b42
Anastasiya <anastasiya.ibr@mail.ru>
2025-08-04 18:05:53 +0300

    feat: доб.скил

--- react-project/src/constants/skills-data.tsx
+++ react-project/src/constants/skills-data.tsx
@@ -12,6 +12,7 @@ import MobXIcon from "../assets/icons/skills/mobx.svg?react";
 import WebpackIcon from "../assets/icons/skills/webpack.svg?react";
 import ExpressIcon from "../assets/icons/skills/express.svg?react";
 import DockerIcon from "../assets/icons/skills/docker.svg?react";
+import JestIcon from "../assets/icons/skills/jest.svg?react";
 
 export const SOFT_SKILLS = [
   {
@@ -34,7 +35,7 @@ export const SOFT_SKILLS = [
     ),
   },
   {
-    name: "Умение планировать",
+    name: "Эффективное планирование",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -71,6 +72,63 @@ export const SOFT_SKILLS = [
       </svg>
     ),
   },
+  {
+    name: "Критическое мышление",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="#000000"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "Навыки переговоров",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="#000000"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "Креативность",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="#000000"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+        />
+      </svg>
+    ),
+  },
 ];
 export const HARD_SKILLS = [
   {
@@ -86,25 +144,18 @@ export const HARD_SKILLS = [
     icon: <JsIcon />,
   },
   {
-    name: "React",
-    icon: <ReactIcon />,
-  },
-  {
-    name: "NodeJS",
-    icon: <NodeIcon />,
-  },
-  {
-    name: "MongoDB",
-    icon: <MongoIcon />,
-  },
-  {
     name: "TS",
     icon: <TsIcon />,
+  },
+  {
+    name: "React",
+    icon: <ReactIcon />,
   },
   {
     name: "NextJS",
     icon: <NextIcon />,
   },
+
   {
     name: "Git",
     icon: <GitIcon />,
@@ -118,15 +169,28 @@ export const HARD_SKILLS = [
     icon: <MobXIcon />,
   },
   {
-    name: "Webpack",
-    icon: <WebpackIcon />,
+    name: "NodeJS",
+    icon: <NodeIcon />,
   },
   {
     name: "Express",
     icon: <ExpressIcon />,
   },
   {
+    name: "MongoDB",
+    icon: <MongoIcon />,
+  },
+  {
+    name: "Webpack",
+    icon: <WebpackIcon />,
+  },
+
+  {
     name: "Docker",
     icon: <DockerIcon />,
+  },
+  {
+    name: "Jest",
+    icon: <JestIcon />,
   },
 ];
